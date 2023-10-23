305e302089a5a6e2aa25da3ce066153cfbc162d2
AlexNova <alexandre.tellier2@gmail.com>
2023-10-23 13:08:26 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import QR_Scanner from './src/tools/Scanner'
 
 export default function App() {
+    const [showQrScanner, setshowQrScanner] = useState(false);
+
+    function returnButtonTitle(){
+        let buttonTitle = "Ouvrir le scanner"
+
+        if (showQrScanner === true) 
+        {
+            buttonTitle = "Fermer le scanner"
+        }
+
+        return buttonTitle
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        {showQrScanner === true ? <QR_Scanner/> : <Text></Text>}
+        <Button
+            title={returnButtonTitle()}
+            color="#f194ff"
+            onPress={() => {setshowQrScanner(!showQrScanner)}}
+        />
     </View>
   );
 }
