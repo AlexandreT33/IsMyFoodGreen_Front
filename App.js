c97e1c2676638cf812ffe208b327ace9159c8dcb
AlexNova <alexandre.tellier2@gmail.com>
2023-10-24 15:07:20 +0200

    imple elements ui

--- App.js
+++ App.js
@@ -15,8 +15,7 @@ export default function App() {
 
     useEffect(() => {
         if (scanValue != null) {
-            
-            fetchBarCodeData(scanValue);      
+            fetchBarCodeData(scanValue);
             setshowQrScanner(false);
         }
         
@@ -25,18 +24,17 @@ export default function App() {
     async function fetchBarCodeData(barcodeValue) {
         try 
         {
-            alert(`COUCOU ${barcodeValue}`);
             const response = await fetch(`https://a070-2a01-cb01-307f-c3aa-526e-b7d5-9e84-30b1.ngrok.io/products/${barcodeValue}`);
             const jsonData = await response.json();
             console.log(jsonData);
         
-              setProductInfo({
+            setProductInfo({
                 abbreviated_product_name: jsonData?.abbreviated_product_name_fr,
                 generic_name: jsonData?.generic_name,
                 brands: jsonData?.brands,
                 images_url: jsonData?.images?.display?.fr,
                 co2_total: jsonData?.eco?.agribalyse?.co2_total,
-              });
+            });
         }
         catch (error) 
         {
@@ -48,30 +46,38 @@ export default function App() {
     
     <View style={{flex:1}}>
         <View style={styles.topbox}>
-            
-            {showQrScanner === true ? <QR_Scanner setScanValue={setScanValue} /> : <></> }
-            <View style={
-                { 
-                    width: imageWidth, 
-                    height: imageHeight,
-                    margin: 20,
-                    marginTop: 50,
+            {showQrScanner === true ? <QR_Scanner setScanValue={setScanValue} /> :  
+            <View style={{flex: 1}}>
+                { productInfo === null ? <></> :
+                    <View style={{flex: 1}}>
+                        <Image
+                            source={{ uri: "https://images.openfoodfacts.org/images/products/306/832/012/4377/front_en.3.400.jpg" }} // Replace with the URL of your image
+                            style={
+                                { 
+                                    width: imageWidth, 
+                                    height: imageHeight,
+                                    margin: 20,
+                                    marginTop: 50,
+                                    borderRadius: 10,
+                                }}
+                        />
+                        <Text style={styles.text} >Nom : {productInfo.generic_name}</Text>
+                        <Text style={styles.text} >Marque : {productInfo.brands}</Text>
+                        <Text style={styles.text} >Total de C02 : {productInfo.co2_total}</Text>
 
-                }}>
-                <Image
-                    source={{ uri: "https://images.openfoodfacts.org/images/products/306/832/012/4377/front_en.3.400.jpg" }} // Replace with the URL of your image
-                    style={{ flex:1 }}
-                />
+                    </View>
+                }
             </View>
-            
+            }
 
         </View>
 
         <View style={styles.bottombox}>
-            {productInfo === null ? <></> : <Text>Brand: {productInfo.brands}</Text> }
+            
             <Button
-                title={showQrScanner === true ? "Ouvrir le scanner": "Fermer le scanner"}
+                title={showQrScanner === true ? "Fermer le scanner" : "Ouvrir le scanner"}
                 color="#008080"
+                
                 onPress={() => {setshowQrScanner(!showQrScanner)}}
             />
         </View>
@@ -80,17 +86,22 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  topbox: {
-    backgroundColor: '#00FF00',
-    flex: 15
-  },
-  topbox_img: {
-    flex: 1,
-    margin: 20,
-  },
-  bottombox: {
-    backgroundColor: '#2E2E2E',
-    flex: 1,
-    padding: 20,
-  }
+    text: {
+        margin: 3,
+        fontWeight: 'bold',
+    },
+    topbox: {
+        backgroundColor: '#D3D3D3',
+        alignItems: 'center',
+        flex: 15
+    },
+    topbox_img: {
+        flex: 1,
+        margin: 20,
+    },
+    bottombox: {
+        backgroundColor: '#2E2E2E',
+        flex: 1,
+        padding: 20,
+    }
 });
